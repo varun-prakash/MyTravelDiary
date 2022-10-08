c8576ad21267496e9418c39f284f66e06167a21d
varun prakash <varunprakash652@gmail.com>
2022-10-08 10:30:19 +0530

    update

--- client/src/Pages/Home/Home.jsx
+++ client/src/Pages/Home/Home.jsx
@@ -1,10 +1,13 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./Home.css";
 import Header from "../../components/Header/Header";
 import Posts from "../../components/posts/posts";
 import Sidebar from "../../components/sidebar/sidebar";
 
 export default function Home() {
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {});
   return (
     <>
       <Header />
